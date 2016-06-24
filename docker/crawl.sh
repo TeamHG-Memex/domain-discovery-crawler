026c0da85bc061f021249fc53d8d80b80404b391
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-06-24 11:44:28 +0300

    Wait for redis

--- docker/crawl.sh
+++ docker/crawl.sh
@@ -5,5 +5,8 @@ set -e
 # Start caching DNS server
 dnsmasq
 
+echo 'Waiting for redis...'
+while ! nc -w 1 -z redis 6379; do sleep 0.1; done
+
 # Crawl
 scrapy crawl dd_crawler -s REDIS_HOST=redis $*
