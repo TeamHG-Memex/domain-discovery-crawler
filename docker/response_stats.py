4b56613eaa6d1adfcdcc6099f8152ed734e96e28
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-08-02 18:18:15 -0400

    Prettier filename for response stats charts

--- docker/response_stats.py
+++ docker/response_stats.py
@@ -5,7 +5,7 @@ from utils import run_in_docker
 
 def main():
     run_in_docker(
-        'scrapy response_stats -o /out/response_stats.html /out/*.csv')
+        'scrapy response_stats -o /out/response-stats /out/*.csv')
 
 
 if __name__ == '__main__':
